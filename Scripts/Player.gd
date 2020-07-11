9d7e121133f2a40019fca5b7f2a96bc49e7b4414
Oleg Jakovlev <38386178+KorossGame@users.noreply.github.com>
2020-07-11 12:27:09 +0300

    Update Player.gd

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -44,12 +44,12 @@ func _physics_process(delta):
 	#Vertical
 	if (Input.is_action_pressed("ui_down")):
 		direction.y = 1;
-    anim_player.play("walk_down")
+    		anim_player.play("walk_down")
 		character.set_rotation_degrees(90);
 	elif (Input.is_action_pressed("ui_up")):
 		direction.y = -1;
-    character.set_rotation_degrees(-90);
-    anim_player.play("walk_up")
+    		character.set_rotation_degrees(-90);
+    		anim_player.play("walk_up")
 	else: direction.y = 0;
 	
 	#Idle Animations
