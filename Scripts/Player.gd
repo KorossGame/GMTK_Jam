6bda95171f7c6fce32b427522f6ccbc94a5d8f9f
CoderEdward <edwardstrazd@gmail.com>
2020-07-11 12:02:36 +0300

    Character & Movement animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,22 +6,43 @@ var bullet = preload("res://Prefabs/Bullet.tscn");
 export var speed = 150;
 var direction = Vector2();
 
+onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+
 #Movement function (called every physics frame)
 func _physics_process(delta):
 
 	#Horizontal
 	if Input.is_action_pressed("ui_right"):
 		direction.x = 1;
+		anim_player.play("walk_right")
 	elif (Input.is_action_pressed("ui_left")):
 		direction.x = -1;
+		anim_player.play("walk_left")
 	else: direction.x = 0;
 	
 	#Vertical
 	if (Input.is_action_pressed("ui_down")):
 		direction.y = 1;
+		anim_player.play("walk_down")
 	elif (Input.is_action_pressed("ui_up")):
 		direction.y = -1;
+		anim_player.play("walk_up")
 	else: direction.y = 0;
+	
+	
+	
+	#Idle Animations
+	if (Input.is_action_just_released("ui_right")):
+		anim_player.play("idle_right")
+	elif (Input.is_action_just_released("ui_left")):
+		anim_player.play("idle_left")
+	elif (Input.is_action_just_released("ui_down")):
+		anim_player.play("idle_down")
+	elif (Input.is_action_just_released("idle_up")):
+		anim_player.play("idle_down")
+	
+	
+	
 	
 	#Move
 	move_and_slide(direction * speed);
@@ -31,8 +52,7 @@ var fire_rate = 0.2;
 var can_fire = true;
 
 func _process(delta):
-	#Looking at mouse possition (called every frame)	
-	look_at(get_global_mouse_position());
+	
 	
 	#Check if player hits fire button
 	if (Input.is_action_pressed("fire") && can_fire):
@@ -45,3 +65,8 @@ func _process(delta):
 		can_fire = !can_fire;
 		yield (get_tree().create_timer(fire_rate), "timeout");
 		can_fire = !can_fire;
+
+
+
+
+
