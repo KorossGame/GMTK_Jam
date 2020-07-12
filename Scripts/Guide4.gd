ee0a8d9eaeac08a92d8ee4f0be10dfd229b69575
Oleg Jakovlev <olegjakovlevlive@gmail.com>
2020-07-12 19:15:40 +0300

    Guide added

---
+++ Scripts/Guide4.gd
@@ -0,0 +1,14 @@
+extends RichTextLabel
+
+onready var anim_player: AnimationPlayer = get_node("AnimationPlayer");
+
+func _on_NextRoom2_body_entered(body):
+	anim_player.play("Fade");
+	yield (get_tree().create_timer(1), "timeout");
+	queue_free()
+
+
+func _on_Bot_tree_exiting():
+	anim_player.play("Fade");
+	yield (get_tree().create_timer(1), "timeout");
+	queue_free()
