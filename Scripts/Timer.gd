98c40dc76222ab5db9f05de6fe7c5a2dae50e000
CoderEdward <edwardstrazd@gmail.com>
2020-07-12 15:25:22 +0300

    Effect icons + timers

--- Scripts/Timer.gd
+++ Scripts/Timer.gd
@@ -1,7 +1,8 @@
 extends Timer
 
 func restoreMovementTimer():
+	get_node("../CanvasLayer/NormalMovement").show()
 	yield (get_tree().create_timer(30), "timeout");
+	get_node("../CanvasLayer/NormalMovement").popup_hide()
 	get_node("../YSort/Player").newMovement()
-
 
